f2f62804af980e9501d0bc10ee22d49321bbdfde
Colin Jaffe <balloonasaurus@gmail.com>
2020-11-19 10:39:18 -0500

    Makes it run without persistence.

--- main.js
+++ main.js
@@ -2,27 +2,34 @@ const readline = require('readline');
 
 
 const interface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
-let names = [];
+const names = [
+    'Mesuara',
+    'Colin',
+    'Anthony',
+];
 
 const randomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const printRandomName = () => {
-    console.log(randomElement(names) + ' is your random name!\n\n');
+    console.log('\n' + randomElement(names) + ' is your random name!\n\n');
     interface.question('Type 1 to get a random name, 2 to add a name to the list. Anything else quits.\n\n', handleAnswer);
 }
 
-const addName = () => {
+const addName = (name) => {
+    names.push(name);
+    console.log('\n' + name + ' has been added!\n')
+    interface.question('Type 1 to get a random name, 2 to add a name to the list. Anything else quits.\n\n', handleAnswer);
 }
 
 const handleAnswer = (answer) => {
     if (answer === '1') {
         printRandomName();
     } else if (answer === '2') {
-        interface.question('What name would you like to add?\n\n', addName)
+        interface.question('\nWhat name would you like to add?\n\n', addName)
     } else {
         console.log('Quitting...');
         interface.close();
