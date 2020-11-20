6b2097eb2cf45ecd18f13c81361dbe490cb7cd21
Timothy Do <tydo83@gmail.com>
2020-11-20 14:24:27 -0500

    almost forgot to upload this

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 const readline = require('readline');
-
+const fs = require('fs');
 
 const interface = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-const names = [
-    'Mesuara',
-    'Colin',
-    'Anthony',
-];
+let names = [];
+
+const fileHasBeenRead = (err, data) => {
+    if(err) { // err !== undefined
+        throw err; // it stops execution, and print out error.  
+    }              // no syntax garbage.
+    const obj = JSON.parse(data)
+    names = obj.names
+    }
+
+fs.readFile('./names.json', fileHasBeenRead)
+
 
 const randomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
@@ -21,8 +28,17 @@ const printRandomName = () => {
 
 const addName = (name) => {
     names.push(name);
-    console.log('\n' + name + ' has been added!\n')
-    interface.question('Type 1 to get a random name, 2 to add a name to the list. Anything else quits.\n\n', handleAnswer);
+    const obj = {
+        names: names,
+    }
+    const data = JSON.stringify(obj, null, 2);
+    fs.writeFile('./names.json', data, 'utf8', (err) => {
+        if(err) {
+            throw err;
+        }
+        console.log(`${name} was added to our database\n\n`);
+        interface.question('Type 1 to get a random name, 2 to add a name to the list. Anything else quits.\n\n', handleAnswer);
+    })
 }
 
 const handleAnswer = (answer) => {
